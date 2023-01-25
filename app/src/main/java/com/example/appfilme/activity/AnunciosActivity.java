f37a4c85c537b12a316f20737062a096483648e9
Anderson Gonçalves <anderson191225@gmail.com>
2023-01-25 15:47:32 -0300

    Publicacoes publicas salvas

--- app/src/main/java/com/example/appfilme/activity/AnunciosActivity.java
+++ app/src/main/java/com/example/appfilme/activity/AnunciosActivity.java
@@ -2,28 +2,83 @@ package com.example.appfilme.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.example.appfilme.R;
+import com.example.appfilme.adapter.AdapterAnuncios;
 import com.example.appfilme.helper.ConfiguracaoFirebase;
+import com.example.appfilme.model.Publicacao;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AnunciosActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao;
+    private RecyclerView recyclerPublicacoesPublicas;
+    private Button buttonCategoria;
+    private AdapterAnuncios adapterAnuncios;
+    private List<Publicacao> listaPublicacoes = new ArrayList<>();
+    private DatabaseReference publicacoesPublicasRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncios);
 
+        inicializarComponentes();
+
+
+
         //Configurações iniciais
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        publicacoesPublicasRef = ConfiguracaoFirebase.getFirebase().child("publicacoes");
+        //Configurar RecyclerView
+        recyclerPublicacoesPublicas.setLayoutManager(new LinearLayoutManager(this));
+        recyclerPublicacoesPublicas.setHasFixedSize(true);
 
+        adapterAnuncios = new AdapterAnuncios(listaPublicacoes, this);
+
+        recyclerPublicacoesPublicas.setAdapter(adapterAnuncios);
+        recuperarPublicacoesPublicas();
         //autenticacao.signOut();
+    }
+    public void recuperarPublicacoesPublicas(){
+        LoadingAlert loadingAlert = new LoadingAlert(AnunciosActivity.this);
+        loadingAlert.startAlertDialog();
+        listaPublicacoes.clear();
+        publicacoesPublicasRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot categorias: dataSnapshot.getChildren()){
+                    for(DataSnapshot publicacoes: categorias.getChildren()){
+                        Publicacao publicacao = publicacoes.getValue(Publicacao.class);
+                        listaPublicacoes.add(publicacao);
+
+                    }
+                }
+                Collections.reverse(listaPublicacoes);
+                adapterAnuncios.notifyDataSetChanged();
+                loadingAlert.closeAlertDialog();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
@@ -63,5 +118,8 @@ public class AnunciosActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void inicializarComponentes(){
+        recyclerPublicacoesPublicas = findViewById(R.id.recyclerPublicacoesPublicas);
     }
 }
